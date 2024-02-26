c9b0b1fbd53d20a25bfb6d9d4a6569dabc326de0
FOURNIER Sébastien <fournier.sebastien@outlook.com>
2024-02-26 18:03:09 +0100

    Create BannerController.php

---
+++ Controller/Front/BannerController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller\Front\Action;
+use App\Controller\Front\FrontController;
+
+/**
+ * BannerController.
+ *
+ * Front Banner renders
+ *
+ * @author Sébastien FOURNIER <contact@sebastien-fournier.com>
+ */
+class BannerController extends FrontController
+{
+
+}
