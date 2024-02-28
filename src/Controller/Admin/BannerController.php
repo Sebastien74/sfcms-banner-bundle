5deebb0be05018ffa9b4364b54ecc3514121e0ff
FOURNIER Sébastien <fournier.sebastien@outlook.com>
2024-02-28 09:46:55 +0100

    Core development

--- src/Controller/Admin/BannerController.php
+++ src/Controller/Admin/BannerController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Sfcms\BannerBundle\Controller\Admin;
+namespace App\Controller\Admin\Module\Admin;
 
 use App\Controller\Admin\AdminController;
 use Symfony\Component\Routing\Attribute\Route;
