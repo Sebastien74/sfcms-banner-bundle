ff9e867ffe2e3c05a9b3f158026c6832771856b6
FOURNIER Sébastien <fournier.sebastien@outlook.com>
2024-03-05 11:03:18 +0100

    Update layout.js

--- assets/js/front/default/layout.js
+++ assets/js/front/default/layout.js
@@ -1,3 +1,4 @@
+/** Banner bundle */
 let banner = body.querySelectorAll('[data-component="banner"]');
 if (banner.length > 0) {
     import(/* webpackPreload: true */ './components/banner').then(({default: banner}) => {
